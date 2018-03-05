82742ce10cac5acc89b4fc9e6fbc367ca576e024
wise king <cosnba@163.com>
2018-03-05 17:10:19 +0800

    1

---
+++ demo.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	a = MyFunc()
+	fmt.Println("a =", a)
+
+	b := MyFunc()
+	fmt.Println("b =", b)
+}
+
+//MyFunc MyFunc
+func MyFunc() int {
+	return 666
+}
